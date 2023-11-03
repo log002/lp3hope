e2f73aa7717e7ee6d086122b308cffc16239744d
log002 <118474240+log002@users.noreply.github.com>
2023-11-03 11:05:27 +0530

    Update iterative_fib.py

--- iterative_fib.py
+++ iterative_fib.py
@@ -1,16 +1,18 @@
-def fabonacci_iterative(n):
-    first=0
-    second=1
+def fib_iterative(n):
+    first = 0
+    second = 1
     print(first)
     print(second)
-    while n-2 > 0:
+    while n - 2 > 0:
         third = first + second
         print(third)
         first = second
         second = third
-        n-=1
-if _name=="main_":
-    n=input("Enter the value of n: ")
-    n=int(n)
+        n -= 1
+
+
+if __name__ == "__main__":
+    n = input("Enter the value of n: ")
+    n = int(n)
     
-    fabonacci_iterative(n)
+    fib_iterative(n)
