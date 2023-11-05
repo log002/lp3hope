9d7d4e6790fd5da777711892b6efae1eeec5c750
log002 <118474240+log002@users.noreply.github.com>
2023-11-05 22:53:16 +0530

    Update branch_and_bound.py

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -1,61 +1,49 @@
 class Item:
-    def __init__(self,value,weight):
-        self.weight=weight
-        self.value=value
-        self.ratio=value/weight
-        
-    
-def knapsack(items,capacity):
-        items.sort(key=lambda x:x.ratio,reverse=True)
-        
-        def bound(i,current_weight,current_value):
-            
-            if(current_weight>=capacity):
-                return 0
-            j=i
-            bound_value=current_value
-            
-            while j<len(items) and current_weight+items[j].weight<capacity:
-                current_weight+=items[j].weight
-                bound_value+=items[j].value
-                j+=1
-                
-            
-            if(j<len(items)):
-                bound_value+=(capacity-current_weight)*items[j].ratio
-                
-            return bound_value
-        
-        def branch_and_bound(i,current_weight,current_value):
-            nonlocal maxvalue
-            
-            if(current_weight<=capacity and current_value>maxvalue):
-                maxvalue=current_value
-                
-            if(i==len(items)):
-                return
-            
-            if(current_weight+items[i].weight<=capacity):
-                branch_and_bound(i+1,current_weight+items[i].weight,current_value+items[i].value)
-                
-            
-            if(bound(i+1,current_weight,current_value)>maxvalue):
-                branch_and_bound(i+1,current_weight,current_value)
-                
-        maxvalue=0
-        branch_and_bound(0,0,0)
-        return maxvalue
-            
-            
-capacity=int(input(" Enter the capacity of knapsack "))
-n=int(input("enter the number of items "))
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+        self.ratio = value / weight
 
-items=[]
-for i in range (0,n):
-    weight=int(input("Enter the weight "))
-    value=int(input("Enter the value "))
-    items.append(Item(value,weight))
+def knapsack(items, cap):
+    items.sort(key = lambda x:x.ratio, reverse = True)
+    def bound(i, curr_wt, curr_val):
+        if(curr_wt >= cap):
+            return 0
+        j = i
+        bound_val = curr_val
+        while j < len(items) and curr_wt + items[j].weight < cap:
+            curr_wt += items[j].weight
+            bound_val += items[j].value
+            j += 1
+        if j < len(items):
+            bound_val += (cap - curr_wt) * items[j].ratio
+        return bound_val
     
+    def branch_and_bound(i, curr_wt, curr_val):
+        nonlocal maxval
+        if(curr_wt <= cap and curr_val > maxval):
+            maxval = curr_val
+        
+        if i == len(items):
+            return
+    
+        if(curr_wt + items[i].weight <= cap):
+            branch_and_bound(i + 1, curr_wt + items[i].weight, curr_val + items[i].value)
+        
+        if(bound(i + 1, curr_wt, curr_val) > maxval):
+            branch_and_bound(i + 1, curr_wt, curr_val)
+    maxval = 0
+    branch_and_bound(0,0,0)
+    return maxval
 
-max_value=knapsack(items,capacity)
-print("max_value = ",max_value)
+cap = int(input("Enter the capacity of knapsack: "))
+n = int(input("Enter the number of items: "))
+
+items = []
+for i in range(n):
+    wt = int(input("Enter the weight "))
+    val = int(input("enter the value" ))
+    items.append(Item(val,wt))
+    
+max_val = knapsack(items, cap)
+print("Max profit is ", max_val)
