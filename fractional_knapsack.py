09f52b9a8dde5e540cc2fda2bde43876792e67b7
log002 <118474240+log002@users.noreply.github.com>
2023-11-05 22:32:52 +0530

    Update fractional_knapsack.py

--- fractional_knapsack.py
+++ fractional_knapsack.py
@@ -1,42 +1,34 @@
 class Item:
-    def _init_(self,value,weight):
-        self.weight=weight
-        self.value=value
-        self.ratio=value/weight
-        
+    def __init__(self, value, weight):
+        self.weight = weight
+        self.value = value
+        self.ratio = value / weight
 
-def knapsack(items,cap):
-    items.sort(key=lambda x:x.ratio,reverse=True)
+def knapsack(items, cap):
+    items.sort(key = lambda x:x.ratio, reverse = True)
     
-    wt=0
-    p=0
-    
+    wt = 0
+    p = 0
     for i in range(len(items)):
-        if cap==0:
+        if(cap == 0):
             break
-        if(items[i].weight<=cap):
-            p+=items[i].value
-            cap-=items[i].weight
-        elif(items[i].weight>cap):
-            p+=cap*items[i].ratio
-            cap=0
-            
-    
+        if items[i].weight <= cap:
+            p += items[i].value
+            cap -= items[i].weight
+        elif items[i].weight > cap:
+            p += cap * items[i].ratio
+            cap = 0
     return p
-
-
-if _name=="main_":
     
-    n=int(input(" Enter the number of items "))
-    cap=int(input(" enter the capacity of knapsack "))
-    items=[]
+if __name__ == "__main__":
+    n = int(input("Enter the number of items: "))
+    cap = int(input("Enter the capacity of knapsack: "))
+    items = []
     
-    for i in range(0,n):
-        w=int(input("Enter the weight "))
-        val=int(input("Enter the value "))
+    for i in range(0, n):
+        w = int(input("enter the weight"))
+        val = int(input("enter the value"))
         
-        items.append(Item(val,w))
+        items.append(Item(val, w))
         
-    
-    
-    print("The max profit will be ",knapsack(items,cap))
+    print("The max profit will be ", knapsack(items, cap))
