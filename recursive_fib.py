1eeda6343712574671748ab0a26bd0cd8d7a4101
log002 <118474240+log002@users.noreply.github.com>
2023-11-05 22:21:23 +0530

    Update recursive_fib.py

--- recursive_fib.py
+++ recursive_fib.py
@@ -1,11 +1,10 @@
 def fib_recursive(n):
     if n <= 1:
-        return n;
+        return n
     else:
         return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 if __name__ == "__main__":
-    n = input("Enter the number :")
-    n = int(n)
+    n = int(input("Enter the value of n :"))
     for i in range(n):
         print(fib_recursive(i))
